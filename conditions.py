735df947d5e8cee1a1c971152b8c85a4cc105b87
Rip_Blue <ripblueskyrh@gmail.com>
2024-11-30 09:08:57 -0500

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,8 @@
+num=int (input("enter a number"))
+
+if num>0:
+    print("inputed number is positive")
+elif num<0:
+    print("inputed numver is negative")
+else:
+    print("the number inputed was 0 😅")
